26b651db7c3b17876db29ec85a83a99e582675ee
NameInikita <name.inikita@gmail.com>
2017-07-22 19:03:09 +0300

    Yield breake, yield return

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+
+namespace ConsoleApplication1
+{
+    class User
+    {
+        public static IEnumerable Method()
+        {
+          //while (true)//бесконечно. MoveNext безусловно возвращает true 
+            yield return "Hello";
+          //yield break;// возвращается пустая коллекция(пустой итератор)
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            foreach (var elem in User.Method())
+                Console.WriteLine(elem);
+
+            Console.WriteLine(new string('=',5));
+
+            foreach (string elem in User.Method())
+                Console.WriteLine(elem);
+
+            Console.ReadKey();
+        }
+    }
+}
